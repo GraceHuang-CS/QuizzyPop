4af0ec4b2c4a4ab11ffd87ce692ae2696c7f65fe
Khaing Chaw Su Latt <115727480+GraceHuang-CS@users.noreply.github.com>
2025-08-19 15:57:14 -0400

    added port listener

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+connectDB();
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}!✅`);
+});
