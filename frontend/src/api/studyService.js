4982af80e1d7c27feb18c0ddc1c029e2998397a7
Khaing Chaw Su Latt <115727480+GraceHuang-CS@users.noreply.github.com>
2025-08-20 09:14:37 -0400

    removed quiz functions

--- frontend/src/api/studyService.js
+++ frontend/src/api/studyService.js
@@ -111,50 +111,50 @@ export const updateMaterial = async (materialId, payload, token) => {
   }
 };
 
-export const submitQuizAttempt = async (materialId, answers, token) => {
-  try {
-    const response = await axios.post(
-      `/api/dashboard/submit-quiz-attempt/${materialId}`,
-      { answers },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (err) {
-    console.error(
-      "Error submitting quiz attempt:",
-      err?.response?.data || err.message
-    );
-    throw new Error(
-      err?.response?.data?.error || "Failed to submit quiz attempt"
-    );
-  }
-};
+// export const submitQuizAttempt = async (materialId, answers, token) => {
+//   try {
+//     const response = await axios.post(
+//       `/api/dashboard/submit-quiz-attempt/${materialId}`,
+//       { answers },
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         withCredentials: true,
+//       }
+//     );
+//     return response.data;
+//   } catch (err) {
+//     console.error(
+//       "Error submitting quiz attempt:",
+//       err?.response?.data || err.message
+//     );
+//     throw new Error(
+//       err?.response?.data?.error || "Failed to submit quiz attempt"
+//     );
+//   }
+// };
 
-export const getQuizAttempts = async (materialId, token) => {
-  try {
-    const response = await axios.get(
-      `/api/dashboard/quiz-attempts/${materialId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (err) {
-    console.error(
-      "Error fetching quiz attempts:",
-      err?.response?.data || err.message
-    );
-    throw new Error(
-      err?.response?.data?.error || "Failed to fetch quiz attempts"
-    );
-  }
-};
+// export const getQuizAttempts = async (materialId, token) => {
+//   try {
+//     const response = await axios.get(
+//       `/api/dashboard/quiz/quiz-attempts/${materialId}`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//         withCredentials: true,
+//       }
+//     );
+//     return response.data;
+//   } catch (err) {
+//     console.error(
+//       "Error fetching quiz attempts:",
+//       err?.response?.data || err.message
+//     );
+//     throw new Error(
+//       err?.response?.data?.error || "Failed to fetch quiz attempts"
+//     );
+//   }
+// };
