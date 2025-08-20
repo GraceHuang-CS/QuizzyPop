4e4ce67b1ccdf39e4f5eeef5efec256eeb5f4ac1
Khaing Chaw Su Latt <115727480+GraceHuang-CS@users.noreply.github.com>
2025-08-20 05:07:23 -0400

    fixed reset email link in email

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -181,7 +181,7 @@ export async function forgotPassword(req, res) {
       subject: "QuizzPop Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        ${process.env.FRONTEND_URL}/reset-password/${resetToken}\n\n
+        http://localhost:5173/reset-password/${resetToken}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
