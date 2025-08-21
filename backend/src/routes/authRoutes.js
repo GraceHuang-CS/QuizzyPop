4e284a909a11b7ce9c3cce191a3bd5f227df9529
Khaing Chaw Su Latt <115727480+GraceHuang-CS@users.noreply.github.com>
2025-08-21 04:54:48 -0400

    added more backend routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,7 +5,9 @@ import {
   forgotPassword,
   resetPassword,
   getProfile,
+  updateProfile,
   googleSignUp,
+  deleteAccount,
 } from "../controllers/authControllers.js";
 import auth from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -16,4 +18,6 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 router.get("/getProfile", auth, getProfile);
+router.put("/getProfile", auth, updateProfile);
+router.delete("/delete", auth, deleteAccount);
 export default router;
