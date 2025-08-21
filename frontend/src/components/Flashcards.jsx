c1a2ccd6ff23fd7e6cca20373c7d656edbe2293b
Khaing Chaw Su Latt <115727480+GraceHuang-CS@users.noreply.github.com>
2025-08-20 21:23:13 -0400

    modified ui design for flashcard

--- frontend/src/components/Flashcards.jsx
+++ frontend/src/components/Flashcards.jsx
@@ -27,42 +27,40 @@ const Flashcards = ({ cards }) => {
     setCurrentCardIndex((prev) => (prev < cards.length - 1 ? prev + 1 : prev));
   };
   return (
-    <div className="flashcards-container">
-      <h3>
-        Flashcards ({currentCardIndex + 1} / {cards.length})
-      </h3>
+    <>
+      <h3>Flashcards</h3>
+      <div className="flashcards-container">
+        <div className="flashcard-single" onClick={handleFlip}>
+          {flippedCards[currentCardIndex] ? (
+            <div className="flashcard-back">
+              <div className="flashcard-content">{card.answer}</div>
+            </div>
+          ) : (
+            <div className="flashcard-front">
+              <div className="flashcard-content">{card.question}</div>
+              <div className="flashcard-label">
+                Click the card to see the answer.
+              </div>
+            </div>
+          )}
+        </div>
 
-      <div className="flashcard-single" onClick={handleFlip}>
-        {flippedCards[currentCardIndex] ? (
-          <div className="flashcard-back">
-            <div className="flashcard-label">Back</div>
-            <div className="flashcard-content">{card.answer}</div>
-          </div>
-        ) : (
-          <div className="flashcard-front">
-            <div className="flashcard-label">Front</div>
-            <div className="flashcard-content">{card.question}</div>
-          </div>
-        )}
+        <div className="flashcard-navigation">
+          <button onClick={handlePrev} disabled={currentCardIndex === 0}>
+            ←
+          </button>
+          <p className="text-in">
+            ({currentCardIndex + 1} / {cards.length})
+          </p>
+          <button
+            onClick={handleNext}
+            disabled={currentCardIndex === cards.length - 1}
+          >
+            →
+          </button>
+        </div>
       </div>
-
-      <div className="flashcard-navigation">
-        <button
-          onClick={handlePrev}
-          disabled={currentCardIndex === 0}
-          className="nav-button"
-        >
-          ← Previous
-        </button>
-        <button
-          onClick={handleNext}
-          disabled={currentCardIndex === cards.length - 1}
-          className="nav-button"
-        >
-          Next →
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
